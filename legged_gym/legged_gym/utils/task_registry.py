01640d1aef732dad93f87551630f1323d30509fd
inkyant <80181074+inkyant@users.noreply.github.com>
2024-05-24 23:05:09 -0700

    use new logs directory

--- legged_gym/legged_gym/utils/task_registry.py
+++ legged_gym/legged_gym/utils/task_registry.py
@@ -153,7 +153,7 @@ class TaskRegistry():
         #save resume path before creating a new log_dir
         resume = train_cfg.runner.resume
         if args.resumeid:
-            log_root = LEGGED_GYM_ROOT_DIR + f"/logs/{args.proj_name}/" + args.resumeid
+            log_root = f"/opt/isaacgym/output_files/logs/{args.proj_name}/" + args.resumeid
             resume = True
         if resume:
             # load previously trained model
